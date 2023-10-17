e16fb6184327b51642bffdcc20fcc9034646855b
waruj1m <james@polyphasicdevs.com>
2023-10-17 15:52:29 +0100

    fixed a bug with the inputs
    got q meaning quit and < > meaning prev and skip, media keys also working now.

--- ytmusiccmus.py
+++ ytmusiccmus.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python3
 import subprocess
 import random
+import os
 from ytmusicapi import YTMusic
+
+
 
 def play_songs_with_mpv(urls, titles):
     """Play multiple songs using mpv with MPRIS support and customized output."""
-    for url, title in zip(urls, titles):
-        print(f"\033[92mNow Playing: {title}\033[0m")  # Display title in green color
-        args = [
-            "mpv",
-            "--no-video",
-            "--no-resume-playback",
-            "--msg-level=cplayer=error",
-            "--msg-level=ao=error",
-            url
-        ]
-        subprocess.run(args)
+    
+    # Create a temporary playlist file
+    playlist = "\n".join(urls)
+    with open("temp_playlist.txt", "w") as f:
+        f.write(playlist)
+    
+    print(f"\033[92mNow Playing: {titles[0]}\033[0m")  # Display the first title in green color
+    args = [
+        "mpv",
+        "--no-video",
+        "--no-resume-playback",
+        "--msg-level=cplayer=error",
+        "--msg-level=ao=error",
+        "--playlist=temp_playlist.txt"  # Play songs from the temporary playlist
+    ]
+    subprocess.run(args)
+    
+    # Clean up the temporary playlist file
+    os.remove("temp_playlist.txt")
 
 def main():
     # Initialize YTMusic with your authentication file
     yt = YTMusic('oauth.json')
     
     # Fetch user's saved playlists
-    playlists = yt.get_library_playlists(limit=10)
+    playlists = yt.get_library_playlists(limit=20)
     
     # Display the top 10 playlists to the user in two columns of 5
     print("Your top 10 playlists:")
